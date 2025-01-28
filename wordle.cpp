fa2941285fd32f410ee22b866a10744ec2133f38
Kyle Vu <ktvu@hbuhsd.edu>
2025-01-27 22:53:54 -0800

    Update and rename classwork.cpp to wordle.cpp

---
+++ wordle.cpp
@@ -0,0 +1,58 @@
+/**
+* listhelpers.hpp has been provided by the instructor as starter code
+* contains the following procedures to work with lists:
+* display, append, insert, remove, length
+*/
+#include "listhelpers.hpp"
+
+#include <cstdlib>   // srand, rand
+#include <ctime>     // time
+#include <iostream>  // std::cin, std::cout
+#include <string>    // std::string, std::getline
+#include <vector>    // std::vector
+
+std::vector<int> createSecret() {
+    // Write this procedure here
+    return std::vector<int>();    // replace this with your code
+}
+
+std::vector<std::string> getHint(std::vector<int> secret, std::vector<int> guess) {
+    // Write this procedure here
+    return {"X", "X", "X", "X"};    // replace this with your code
+}
+
+bool winGame(std::vector<int> secret, std::vector<int> guess) {
+    // Write this procedure here
+    return false;    // replace this with your code
+}
+
+int main()
+{
+    std::vector<int> secret_code = createSecret();
+    std::vector<int> user_guess = {};
+    std::vector<std::string> hint = {};    // an empty list
+
+    int secret_code_length = 4;
+    int num_guesses = 0;
+
+    std::cout << "Welcome to Number Wordle!\n";
+    
+    while (!winGame(secret_code, user_guess))    // while you have not won the game yet
+    {
+        std::cout << "\nEnter your guess: ";
+        hint = {};    // reset the hint for the next guess
+        for (int counter = 0; counter < secret_code_length; counter = counter + 1)
+        {
+            int input;
+            std::cin >> input;
+            guess.push_back(input);    // can also do append(guess, input);
+        }
+
+        hint = getHint(secret_code, user_guess);
+        display(hint);
+        num_guesses = num_guesses + 1;
+    }
+
+    std::cout << "Congrats!  You guessed the code!  It took you " << num_guesses << " tries.\n";
+    return 0;
+}
